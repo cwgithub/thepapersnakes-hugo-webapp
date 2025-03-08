b91e1042e5c4235cc5363715b5f913e0ccc55cc7
Chris Ward <cw@yellowdave.co.uk>
2025-03-08 17:33:07 +0000

    Remove countdown code

--- themes/infinity/static/js/main.js
+++ themes/infinity/static/js/main.js
@@ -35,20 +35,4 @@ $(document).ready(function () {
         pagination: true,
         autoPlay: 5000,
     });
-
-    $year = $('#countdown_dashboard').data('year');
-    $month = $('#countdown_dashboard').data('month');
-    $day = $('#countdown_dashboard').data('day');
-    $('#countdown_dashboard').countDown({
-        targetDate: {
-            'day': $day,
-            'month': $month,
-            'year': $year,
-            'hour': 23,
-            'min': 59,
-            'sec': 59,
-        },
-        omitWeeks: true
-    });
-
 });
